e8e4be569a687c5cce22cb907d33f5c799eb4542
nodooo <nodari.gelashvili97@gmail.com>
2025-11-11 17:28:16 +0400

    add app js file

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let name = "nodar";
+
+let age = 28;
+let temperature = 23.5;
+
+let isStudent = true;
+
+let dayOfWeek = "Friday";
+
+let favoriteActivity = "Watching Anime";
+
+let fixText = "Hi, my name is " + name + "! Today is " + dayOfWeek + ", a perfect day for " + favoriteActivity + ".";
+
+
+console.log("name:", name);
+console.log("age:", age);
+console.log("temperature:", temperature);
+console.log("student?:", isStudent);
+console.log("day of week:", dayOfWeek);
+console.log("favourite acivity:", favoriteActivity);
+console.log(fixText);
